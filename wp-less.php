a96ebbd1e1dbecc7ed32e68c1e026813d6df79a5
magicmoose <ralf@scardiecat.org>
2011-12-14 22:33:00 -0500

    removed cache directory. Cache directly at source location use a cascading less compile

--- wp-less.php
+++ wp-less.php
@@ -40,17 +40,18 @@ if ( ! class_exists( 'wp_less' ) ) {
 			$src_path = WP_CONTENT_DIR . '/' . $src_bits[ 2 ];
 
 			// cache file name
-			$cache_path = $this->get_cache_dir() . "/$handle.css";
+			$cache_path = str_replace(".less",".css",$src_path);
 
 			// ccompile automatically regenerates files if source's modified time has changed
 			try {
-				lessc::ccompile( $src_path, $cache_path );
+				//lessc::ccompile( $src_path, $cache_path );
+				$this->auto_compile_less( $src_path, $cache_path );
 			} catch ( exception $ex ) {
 				wp_die( $ex->getMessage() );
 			}
 
 			// return the compiled stylesheet with the query string it had if any
-			return $this->get_cache_dir( false ) . "/$handle.css" . ( isset( $src_bits[ 3 ] ) ? $src_bits[ 3 ] : '' );
+			return get_stylesheet_directory_uri(). "/$handle.css" . ( isset( $src_bits[ 3 ] ) ? $src_bits[ 3 ] : '' );
 
 		}
 
@@ -72,6 +73,22 @@ if ( ! class_exists( 'wp_less' ) ) {
 			}
 
 			return $dir;
+		}
+		
+		function auto_compile_less($less_fname, $css_fname) {
+		  // load the cache
+		  $cache_fname = $less_fname.".cache";
+		  if (file_exists($cache_fname)) {
+		    $cache = unserialize(file_get_contents($cache_fname));
+		  } else {
+		    $cache = $less_fname;
+		  }
+		
+		  $new_cache = lessc::cexecute($cache);
+		  if (!is_array($cache) || $new_cache['updated'] > $cache['updated']) {
+		    file_put_contents($cache_fname, serialize($new_cache));
+		    file_put_contents($css_fname, $new_cache['compiled']);
+		  }
 		}
 
 	}
